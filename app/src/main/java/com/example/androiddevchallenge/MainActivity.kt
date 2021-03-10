388778ec692909564e370dee68c1236c6c7e2453
Muhammad Farhan <mfarhan.jp@gmail.com>
2021-03-10 23:56:07 +0500

    Finilize

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,7 +18,10 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,8 +29,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
@@ -40,7 +42,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,6 +58,7 @@ import com.example.androiddevchallenge.utils.Counter
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -65,6 +73,7 @@ private const val ONE_MINUTE_IN_MILLIS = 60000L
 private const val COUNT_DOWN_INTERVAL = 1000L
 
 // Start building your app here!
+@ExperimentalAnimationApi
 @Composable
 fun MyApp() {
     Surface(color = MaterialTheme.colors.background) {
@@ -72,34 +81,29 @@ fun MyApp() {
     }
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun MainContent() {
-    val progress = remember { mutableStateOf(0f) }
-    val animatedProgress by animateFloatAsState(
-        targetValue = progress.value,
-        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
-    )
+    val progress = remember { mutableStateOf(1.0f) }
     val buttonState = remember { mutableStateOf("Start") }
     val textState = remember { mutableStateOf("60") }
+    val visible = remember { mutableStateOf(true) }
 
     val countDownTimer =
         Counter(
             millisInFuture = ONE_MINUTE_IN_MILLIS, countDownInterval = COUNT_DOWN_INTERVAL,
             {
                 // On Tick
-
-                // 0.001 * 60 * 1000
-                // 0.06
-
-                progress.value = (it / 1000 / 1000).toFloat()
+                progress.value = it.toFloat() / ONE_MINUTE_IN_MILLIS.toFloat()
 
                 textState.value = TimeUnit.MILLISECONDS.toSeconds(it).toString()
             },
             {
                 // On Finish
-                progress.value = 0f
+                progress.value = 1.0f
                 buttonState.value = "Start"
                 textState.value = "60"
+                visible.value = true
             }
         )
     Column(
@@ -109,15 +113,18 @@ fun MainContent() {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        MyCircularProgressIndicator(animatedProgress, textState.value)
+        MyCircularProgressIndicator(progress.value, textState.value)
         Spacer(Modifier.height(30.dp))
-        CounterStateButton(buttonState.value) {
-            if (buttonState.value == "Start") {
-                countDownTimer.start()
-            } else {
-                countDownTimer.cancel()
+        AnimatedVisibility(visible = visible.value) {
+            CounterStateButton(buttonState.value) {
+                if (buttonState.value == "Start") {
+                    countDownTimer.start()
+                    visible.value = false
+                } else {
+                    visible.value = true
+                }
+                buttonState.value = it
             }
-            buttonState.value = it
         }
     }
 }
@@ -134,19 +141,34 @@ fun CounterStateButton(buttonTextState: String, onClickButton: (String) -> Unit)
 }
 
 @Composable
-fun MyCircularProgressIndicator(animatedProgress: Float, timerTextState: String) {
+fun MyCircularProgressIndicator(progress: Float, timerTextState: String) {
+    val animatedProgress by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = ProgressIndicatorDefaults.ProgressAnimationSpec
+    )
     Box(
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
     ) {
+
+        CircularProgressIndicatorBackGround(
+            modifier = Modifier
+                .height(350.dp)
+                .width(350.dp),
+            color = colorResource(R.color.purple_500),
+            stroke = 7
+        )
+
         CircularProgressIndicator(
             modifier = Modifier
-                .padding(16.dp)
-                .size(400.dp),
+                .height(350.dp)
+                .width(350.dp),
             progress = animatedProgress,
+            color = colorResource(R.color.light_grey),
             strokeWidth = 7.dp
         )
+
         Text(
-            modifier = Modifier.padding(bottom = 50.dp),
             style = TextStyle(
                 color = Color.Black,
                 fontWeight = FontWeight.Bold,
@@ -157,6 +179,35 @@ fun MyCircularProgressIndicator(animatedProgress: Float, timerTextState: String)
     }
 }
 
+@Composable
+fun CircularProgressIndicatorBackGround(
+    modifier: Modifier = Modifier,
+    color: Color,
+    stroke: Int
+) {
+    val style = with(LocalDensity.current) { Stroke(stroke.dp.toPx()) }
+
+    Canvas(modifier = modifier, onDraw = {
+
+        val innerRadius = (size.minDimension - style.width)/2
+
+        drawArc(
+            color = color,
+            startAngle = 0f,
+            sweepAngle = 360f,
+            topLeft = Offset(
+                (size / 2.0f).width - innerRadius,
+                (size / 2.0f).height - innerRadius
+            ),
+            size = Size(innerRadius * 2, innerRadius * 2),
+            useCenter = false,
+            style = style
+        )
+
+    })
+}
+
+@ExperimentalAnimationApi
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun LightPreview() {
@@ -165,11 +216,12 @@ fun LightPreview() {
     }
 }
 
-/*
+
+@ExperimentalAnimationApi
 @Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
 fun DarkPreview() {
     MyTheme(darkTheme = true) {
         MyApp()
     }
-}*/
+}
